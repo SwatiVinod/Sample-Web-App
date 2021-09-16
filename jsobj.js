95dbdc2f1d26ef09df472bc531d5f451c5990047
sangee30 <35099922+sangee30@users.noreply.github.com>
2021-09-16 11:43:34 +0530

    Update jsobj.js

--- jsobj.js
+++ jsobj.js
@@ -32,37 +32,49 @@ function spyfall() {
   window.location.href = "https://www.spyfall.app/"
 }
 
+function openStressBuster(){
+    window.location.href = "stressBuster.html";
+}
+
+function pictionary(){
+    window.location.href = "https://skribbl.io/"
+}
+
+function bingo(){
+     window.location.href = "https://myfreebingocards.com/mfbc"
+}
+
 function updateName() {
   let name = '';
   if(app.isPrivateDataAvailable && app.context.getUser())
     name = app.context.getUser().displayName;
   document.getElementById("Username").innerHTML="Welcome "+ name;
 }
-cast =[
-    {  Name:"Millie Bobby Brown", DOB: "19/02/2004", Age: 17,  Place: "Marbella, Spain", 
-     Cast: "Eleven" },
+// cast =[
+//     {  Name:"Millie Bobby Brown", DOB: "19/02/2004", Age: 17,  Place: "Marbella, Spain", 
+//      Cast: "Eleven" },
     
-    { Name:"Noah Schnapp", DOB: "03/10/2004", Age: 16,   Place:"Scarsdale, New York",  
-     Cast: "Will Byers" },
+//     { Name:"Noah Schnapp", DOB: "03/10/2004", Age: 16,   Place:"Scarsdale, New York",  
+//      Cast: "Will Byers" },
     
-    { Name:"Finn Wolfhard", DOB: "23/12/2002", Age: 18,  Place: "Vancouver, Canada", 
-     Cast: "Mike Wheeler" },
+//     { Name:"Finn Wolfhard", DOB: "23/12/2002", Age: 18,  Place: "Vancouver, Canada", 
+//      Cast: "Mike Wheeler" },
 
-    { Name:"Joe Keery", DOB: "24/04/1992", Age: 18,  Place: "Newburyport, Massachusetts", 
-     Cast: "Steve Harrington" },
+//     { Name:"Joe Keery", DOB: "24/04/1992", Age: 18,  Place: "Newburyport, Massachusetts", 
+//      Cast: "Steve Harrington" },
     
-    { Name:"Sadie Sink", DOB: "24/04/1992", Age: 18,  Place: "Newburyport, Massachusetts", 
-     Cast: "Mad Max" },
+//     { Name:"Sadie Sink", DOB: "24/04/1992", Age: 18,  Place: "Newburyport, Massachusetts", 
+//      Cast: "Mad Max" },
 
-    { Name:"Natalie Dyer", DOB: "24/04/1992", Age: 18,  Place: "Newburyport, Massachusetts", 
-     Cast: "Nancy Wheeler" },
+//     { Name:"Natalie Dyer", DOB: "24/04/1992", Age: 18,  Place: "Newburyport, Massachusetts", 
+//      Cast: "Nancy Wheeler" },
     
-];
+// ];
 
-function out(n)
-{
-  var a=" ";
-  for(x in cast[n])
-        a="The role '" +cast[n]["Cast"] + "' is enacted by " + cast[n]["Name"] + " who was born on " + cast[n]["DOB"] + " at " + cast[n]["Place"] + " who's current age now is " + cast[n]["Age"] + ".<br>";
-  document.getElementById("list").innerHTML=a;
-}  
+// function out(n)
+// {
+//   var a=" ";
+//   for(x in cast[n])
+//         a="The role '" +cast[n]["Cast"] + "' is enacted by " + cast[n]["Name"] + " who was born on " + cast[n]["DOB"] + " at " + cast[n]["Place"] + " who's current age now is " + cast[n]["Age"] + ".<br>";
+//   document.getElementById("list").innerHTML=a;
+// }  
